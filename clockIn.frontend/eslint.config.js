b80cc8de0470adadef65f34e6022d65ade0addc1
mykle23-g9 <mykle23.dev@gmail.com>
2024-10-13 01:59:12 +0200

    Configuracion eslint, agregar react plugin

--- clockIn.frontend/eslint.config.js
+++ clockIn.frontend/eslint.config.js
@@ -6,7 +6,7 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import prettier from 'eslint-plugin-prettier';
 import { defineFlatConfig } from 'eslint-define-config';
-// import react from 'eslint-plugin-react';
+import react from 'eslint-plugin-react';
 
 export default defineFlatConfig([
     {
@@ -25,15 +25,21 @@ export default defineFlatConfig([
             'react-hooks': reactHooks,
             'react-refresh': reactRefresh,
             prettier: prettier,
-            // react: react,
+            react: react,
         },
         rules: {
             ...js.configs.recommended.rules,
             ...tsLint.configs['recommended'].rules,
             ...reactHooks.configs.recommended.rules,
-            // ...react.configs.recommended.rules,
+            ...react.configs.flat.recommended.rules,
             'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
             ...prettier.configs.recommended.rules,
+            ...react.configs.flat['jsx-runtime'].rules,
+        },
+        settings: {
+            react: {
+                version: 'detect',
+            },
         },
     },
 ]);
