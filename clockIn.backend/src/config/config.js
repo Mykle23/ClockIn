bc7142d5bc8d9c87d42d718c9ef336fda8706b56
Mykhaylo Lelgant <mykle23.dev@gmail.com>
2024-11-26 13:28:25 +0100

    Init backend

---
+++ clockIn.backend/src/config/config.js
@@ -0,0 +1,14 @@
+const dotenv = require("dotenv");
+const path = require("path");
+
+dotenv.config({ path: path.join(__dirname, "../../.env") });
+
+const port = process.env.PORT;
+const mongoUrl = process.env.MONGODB_URL;
+
+module.exports = {
+  port: port,
+  mongoose: {
+    url: mongoUrl,
+  },
+};
